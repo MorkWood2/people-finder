49c07b43f64cdf0932e9b85af50aea86956a7fa9
Mark Wild <mark.wild189@gmail.com>
2020-01-07 13:39:09 -0800

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -28,14 +29,32 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
+    //destructuring
     const { name, email, password } = req.body;
+
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400.json({msg:'User already exists'}))
+        return res.status(400).json({ msg: 'User already exists' });
       }
-    } catch (err) {}
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('user saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error ');
+    }
   }
 );
 
