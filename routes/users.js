79306d5d6e3ae1872a1266de0a54e11aad66e09f
Mark Wild <mark.wild189@gmail.com>
2020-01-06 19:44:34 -0800

    commit

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const User = require('../models/User');
 router.post(
   '/',
   [
+    //check(field I want to check is "name", then msg u want)
     check('name', 'Please add Name')
       .not()
       .isEmpty(),
@@ -22,13 +23,19 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400.json({msg:'User already exists'}))
+      }
+    } catch (err) {}
   }
 );
 
