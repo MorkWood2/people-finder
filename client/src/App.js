3da38d4a5b181605dd81676f8f1fc62bdbde535c
Mark Wild <mark.wild189@gmail.com>
2020-01-16 12:22:17 -0800

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
 const App = () => {
   return (
     <Router>
